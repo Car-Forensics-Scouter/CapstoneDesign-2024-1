6ff9fd6bb197b440cb5ca7a8e6f221c27650c9ee
limchansu <121649620+limchansu@users.noreply.github.com>
2024-04-09 15:58:38 +0900

    Update LogIn.js

--- frontend/logger-react-app/src/pages/LogIn.js
+++ frontend/logger-react-app/src/pages/LogIn.js
@@ -1,70 +1,82 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { Button } from '@mui/material'
+import Box from '@mui/material/Box'
+import TextFields from '@mui/material/TextField'
 
 const LoginForm = () => {
   const [email, setEmail] = useState('');
   const [id, setID] = useState('');
-  const [passwd, setPasswd] = useState('');
+  const [password, setPassword] = useState('');
 
   const navigate = useNavigate();
-  const goMain = () => {
-    navigate('/MainBoard');
-  };
 
   const handleLogin = async (e) => {
     e.preventDefault();         // form 컴포넌트에서 버튼을 클릭하면 생기는 새로고침을 막아줌.
-    console.log("Username : ${username}, Password: ${password}");
 
-    // reponse 변수는 백엔드 서버의 로그인 로직과 통신합니다.
-    const response = await fetch(
-        "로그인 서버 주소",
+    try{
+        // reponse 변수는 백엔드 서버의 로그인 로직과 통신합니다.
+        const response = await fetch(
+        '로그인 서버 주소',
         {
-          method: "POST",
+          method: 'POST',
           headers: {
-            "Content-Type": "application/json",
+            'Content-Type': 'application/json',
           },
           body: JSON.stringify({
             email: email,
-            password: passwd,
+            id : id,
+            password: password,
           }),
         }
       )
-      .then(res=>res.json())
-      .then(res=>{
-        console.log(res);
-      });
-
-      const result = await response.json();
+      if (response.ok) {
+        const result = await response.json();
+        console.log(result);
+        
+        navigate('/MainBoard');
+      }
+      else {
+        const errorData = await response.json();
+        alert('로그인 실패. 아이디와 비밀번호를 확인해주세요.');
+      }
+    } catch (error) {
+        console.error('로그인 요청 중 오류 발생 : ', error);
+        alert('로그인 요청 중 오류가 발생했습니다.');
+    }
   };
-
 
   return (
     <div>
-        <form className="login-form" onSubmit={handleLogin}>
+        <form className='login-form' onSubmit={handleLogin}>
             <div>
                 <label htmlFor='username'> 아이디 </label>
-                <input
-                    type="text"
-                    placeholder="input ID"
-                    value={id}
-                    onChange={(e) => setID(e.target.value)}
-                />
+                <Box sx={{ marginBottom : 2 }}>
+                    <TextFields
+                        type='text'
+                        placeholder='ID'
+                        value={id}
+                        onChange={(e) => setID(e.target.value)}
+                    />
+                </Box>
             </div>
         
             <div>
                 <label htmlFor='password'> 비밀번호 </label>
-                <input
-                    type="passwd"
-                    placeholder="input Password"
-                    value={passwd}
-                    onChange={(e) => setPasswd(e.target.value)}
-                />
-
-                <button type={handleLogin} onClick={goMain}>로그인</button>
+                <Box sx={{ marginBottom : 2 }}>
+                    <TextFields
+                        type='password'
+                        placeholder='Password'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <Button type='submit' variant='contained' sx={{ marginLeft : 1 }}>
+                        로그인 </Button>
+                </Box>
             </div>
 
-            <p className='signup-link'>
-                아직 회원이 아니신가요? <Link to="/SignUpForm">회원가입</Link>
+            <p className='SignUpForm-link'>
+                아직 회원이 아니신가요? <Link to='/SignUpForm'>회원가입</Link>
             </p>
         </form>
     </div>
