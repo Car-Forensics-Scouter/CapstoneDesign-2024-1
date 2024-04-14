7348c803e0b0a3d886610ef5bf7dfe9f7b32dd87
WookJaes <dnrwo09@gmail.com>
2024-04-14 15:38:26 +0900

    [ADD] - PACKAGE CREATE

---
+++ backend/src/main/java/com/cfs/obd2logger/Obd2loggerApplication.java
@@ -0,0 +1,13 @@
+package com.cfs.obd2logger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Obd2loggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Obd2loggerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/cfs/obd2logger/Obd2loggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cfs.obd2logger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Obd2loggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
