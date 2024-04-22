b191166d8a4ecd7e4077e7692edb8f77dd5ad7f6
SeongbinCho01 <whtjdqls01@gmail.com>
2024-04-23 01:53:14 +0900

    Feat : OBD 및 GPS 데이터 수집 구현

--- raspberrypi/receiveGPS.py
+++ raspberrypi/receiveGPS.py
@@ -1,3 +1,15 @@
 from gps import *
+import time
 
-gpsd = gps()
+gpsd = gps(mode=WATCH_ENABLE|WATCH_NEWSTYLE)
+
+file_path = "./gps_log.txt"
+
+while True:
+    report = gpsd.next()
+    if report["class"] == "TPV":
+        lat = getattr(report, "lat", 0.0)  # 문자열로의 변환이 필요할 수 있음
+        lon = getattr(report, "lon", 0.0)
+    with open(file_path, "w") as file:
+        file.write(lat + "\n" + lon)
+    time.sleep(1)
--- raspberrypi/scanOBDData.py
+++ raspberrypi/scanOBDData.py
@@ -7,8 +7,7 @@ import time
 # GPS도 보내주기 - 라이브러리의 파이썬 버전 호환 문제로 파일에 적어서 교환하는 식으로...
 # VIN은 맨 처음에
 # 제품 일련번호도 맨처음에
-
-json_dict = {}  # 데이터를 저장할 변수 
+# 동영상 전송 부분도 구현해야 함
 
 connection = obd.OBD()  # 차량과 연결
 
@@ -16,44 +15,47 @@ VIN = str(connection.query(obd.commands.VIN).value.magnitude)  # 차량의 VIN
 
 serialNumber = "F1234"  # 제품 일련번호
 
-json_dict = {
-    "VIN" : VIN,
-    "serialNumber" : serialNumber
-}
-
-
-
-
-
-"""
-json_dict["partition1"] = {
-    "SPEED" : 12,
-    "RPM" : 12,
-    "ENGINELOAD" : 12,
-    "FUELLEVEL" : 12,
-    "COOLANTTEMP" : 12,
-    "THROTTLEPOSITION" : 12,
-    "DISTANCE" : 12,
-    "RUNTIME" : 12,
-    "BAROMETRIC" : 12
-}
-"""
-
-
-"""
-now = datetime.datetime.now()
-timestamp = now.strftime("%Y%m%d%H%M%S")
-print(timestamp)
-
-connection = obd.commands.SPEED
+file_path = "./gps_log.txt"
 
 while True:
-    response = connection.query(cmd)
+    json_dict = {
+        "VIN" : VIN,
+        "serialNumber" : serialNumber
+    }
+    for i in range(60):
+        now = datetime.datetime.now()
+        timestamp = now.strftime("%Y%m%d%H%M%S")
+        with open(file_path, "r") as file:
+            lat = file.readline().rstrip('\n')
+            lon = file.readline()
+        SPEED = str(connection.query(obd.commands.SPEED).value.magnitude)
+        RPM = str(connection.query(obd.commands.RPM).value.magnitude)
+        ENGINELOAD = str(connection.query(obd.commands.ENGINE_LOAD).value.magnitude)
+        FUELLEVEL = str(connection.query(obd.commands.FUEL_LEVEL).value.magnitude)
+        COOLANTTEMP = str(connection.query(obd.commands.COOLANT_TEMP).value.magnitude)
+        THROTTLEPOSITION = str(connection.query(obd.commands.THROTTLE_POS).value.magnitude)
+        RUNTIME = str(connection.query(obd.commands.VIN).value.magnitude)
+        BAROMETRIC = str(connection.query(obd.commands.VIN).value.magnitude)
 
-    print(response.value)
-"""
-# 1초마다 OBD 데이터를 수집
-# 타임스탬프도 찍기
-# GPS도 보내주기
-# VIN은 맨 처음에
-# 제품 일련번호도 맨처음에
+        key = "partition" + str(i + 1)
+
+        json_dict[key] = {
+            "timestamp" : timestamp,
+            "SPEED" : SPEED,
+            "RPM" : RPM,
+            "COOLANTTEMP" : COOLANTTEMP,
+            "THROTTLEPOSITION" : THROTTLEPOSITION,
+            "RUNTIME" : RUNTIME,
+            "BAROMETRIC" : BAROMETRIC,
+            "gps_lat" : lat,
+            "gps_lon" : lon
+        }
+
+        time.sleep(1)
+    
+    # 전송 부분 구현
+    print(json_dict)  # test
+
+
+
+# DISTANCE는 백엔드에서 처리
