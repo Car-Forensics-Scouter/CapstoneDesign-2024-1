229958d5c69ad3c2911fc8edd14a8ea6cee611e7
YejinY00n <yoon17710@gmail.com>
2024-05-21 17:45:46 +0900

    Revert "Refactor : S3 설정 수정(Revert)"
    This reverts commit 632d39c7b6c272d833c21520366a5e7b07f111a9.

--- backend/src/main/java/com/cfs/obd2logger/config/S3Config.java
+++ backend/src/main/java/com/cfs/obd2logger/config/S3Config.java
@@ -4,7 +4,7 @@ package com.cfs.obd2logger.config;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -14,19 +14,19 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class S3Config {
 
+  public static final String videoFolder = "video";
+  @Value("${cloud.aws.s3.regin.static}")
+  private String region;
   @Value("${cloud.aws.credentials.access-key}")
   private String accessKey;
   @Value("${cloud.aws.credentials.secret-key}")
   private String secretKey;
-  @Value("${cloud.aws.region.static}")
-  private String region;
 
   // AWS S3 Client 생성
   @Bean
-  public AmazonS3Client amazonS3Client() {
+  public AmazonS3 amazonS3() {
     AWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
-
-    return (AmazonS3Client) AmazonS3ClientBuilder.standard()
+    return AmazonS3ClientBuilder.standard()
         .withRegion(region)
         .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
         .build();
