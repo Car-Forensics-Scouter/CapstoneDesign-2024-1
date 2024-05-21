5d6b3200550f85e895e89aa04f793d9317b32001
YejinY00n <yoon17710@gmail.com>
2024-05-21 17:33:52 +0900

    Feat : Thumbnail 서비스

---
+++ backend/src/main/java/com/cfs/obd2logger/service/ThumbnailService.java
@@ -0,0 +1,57 @@
+package com.cfs.obd2logger.service;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import org.jcodec.api.FrameGrab;
+import org.jcodec.common.io.NIOUtils;
+import org.jcodec.common.model.Picture;
+import org.jcodec.scale.AWTUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+
+@Service
+public class ThumbnailService {
+
+  @Autowired
+  private S3Service s3Service;
+
+  /**
+   * 동영상에서 썸네일 추출
+   */
+  public File generateVideoThumbnail(MultipartFile videoMultiFile, String fileName)
+      throws IOException {
+    // Video 변환 (Multipartfile --> File)
+    File videoFile = new File(fileName);
+    videoMultiFile.transferTo(videoFile);
+
+    // 썸네일 파일 생성 (파일명.png)
+    String thumbExtension = "png";
+    File thumbnail = File.createTempFile(fileName,
+        "." + thumbExtension);
+
+    try {
+      FrameGrab frameGrab = FrameGrab.createFrameGrab(NIOUtils.readableChannel(videoFile));
+      frameGrab.seekToSecondPrecise(0);       // 0초의 프레임
+      Picture thumbnailPic = frameGrab.getNativeFrame();
+
+      BufferedImage bi = AWTUtil.toBufferedImage(thumbnailPic);
+      ImageIO.write(bi, thumbExtension, thumbnail);
+      return thumbnail;
+    } catch (Exception e) {
+      return null;            // TODO : 에러 처리
+    }
+  }
+
+  /**
+   * 썸네일 업로드 후 썸네일 URL 반환
+   */
+  public String uploadThumbnail(File thumbnail, String thumbnailName, String deviceId)
+      throws IOException {
+    return s3Service.uploadFile(thumbnail, deviceId);
+  }
+
+}
