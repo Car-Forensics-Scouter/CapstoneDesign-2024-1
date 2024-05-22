8649b7484e88253e863e4697a1d03029ee69c0f7
YejinY00n <yoon17710@gmail.com>
2024-05-22 15:27:34 +0900

    Refactor : S3 설정 정보 수정 (ver3)

--- backend/src/main/java/com/cfs/obd2logger/config/S3Config.java
+++ backend/src/main/java/com/cfs/obd2logger/config/S3Config.java
@@ -4,7 +4,7 @@ package com.cfs.obd2logger.config;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -14,8 +14,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class S3Config {
 
-  public static final String videoFolder = "video";
-  @Value("${cloud.aws.s3.regin.static}")
+  @Value("${cloud.aws.region.static}")
   private String region;
   @Value("${cloud.aws.credentials.access-key}")
   private String accessKey;
@@ -24,9 +23,9 @@ public class S3Config {
 
   // AWS S3 Client 생성
   @Bean
-  public AmazonS3 amazonS3() {
+  public AmazonS3Client amazonS3() {
     AWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
-    return AmazonS3ClientBuilder.standard()
+    return (AmazonS3Client) AmazonS3ClientBuilder.standard()
         .withRegion(region)
         .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
         .build();
