f9b22ddbdb6d54ea0f8617199a36f156ebf55edc
limchansu <121649620+limchansu@users.noreply.github.com>
2024-06-03 11:21:10 +0900

    Update App.js

--- frontend/logger-react-app/src/App.js
+++ frontend/logger-react-app/src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
 import Banner from './components/Banner';
 import LogIn from './pages/LogIn';
+import FindIdPassword from './pages/FindIdPassword';
 import Reports from './pages/Reports';
 import Library from './pages/Library';
 import GraphDashboard from './pages/GraphDashboard';
@@ -13,6 +13,8 @@ import car1 from "./assets/car1.jpeg";
 
 function App() {
   const [photo, setPhoto] = useState(car1);
+  const [carName, setCarName] = useState();
+  const [serialNumber, SetSerialNumber] = useState();
 
   return (
     <BrowserRouter>
@@ -21,6 +23,7 @@ function App() {
           <Banner/>
           <Routes>
             <Route path="/" element={<LogIn/>}></Route>
+            <Route path="FindIdPassword" element={<FindIdPassword/>}></Route>
             <Route path="Reports" element={<Reports photo={photo}/>}></Route>
             <Route path="Library" element={<Library/>}></Route>
             <Route path="GraphDashboard" element={<GraphDashboard/>}></Route>
