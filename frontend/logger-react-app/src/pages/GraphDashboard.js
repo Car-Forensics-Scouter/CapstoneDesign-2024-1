44c819ba0e1c81abac53f5ad98c36cb1e76ba10a
limchansu <121649620+limchansu@users.noreply.github.com>
2024-06-07 13:20:15 +0900

    Add files via upload

--- frontend/logger-react-app/src/pages/GraphDashboard.js
+++ frontend/logger-react-app/src/pages/GraphDashboard.js
@@ -4,10 +4,45 @@ import Chart from "react-apexcharts";
 import axios from "axios";
 import { errorAlert } from "../components/alert";
 
+const defaultData = [
+    {
+        "deviceId": "-",
+        "timeStamp": "2024-01-01T00:00:00",
+        "vin": "-",
+        "speed": 0.0,
+        "rpm": 0.0,
+        "engineLoad": 0.0,
+        "fuelLevel": 0.0,
+        "barometricPressure": 0.0,
+        "coolantTemp": 0.0,
+        "throttlePos": 0.0,
+        "distance": 0.0,
+        "runTime": 0.0,
+        "lon": 0.0,
+        "lat": 0.0
+    },
+    {
+        "deviceId": "-",
+        "timeStamp": "2024-01-01T00:01:00",
+        "vin": "-",
+        "speed": 0.0,
+        "rpm": 0.0,
+        "engineLoad": 0.0,
+        "fuelLevel": 0.0,
+        "barometricPressure": 0.0,
+        "coolantTemp": 0.0,
+        "throttlePos": 0.0,
+        "distance": 0.0,
+        "runTime": 0.0,
+        "lon": 0.0,
+        "lat": 0.0
+    }
+]
+
 function GraphDashboard() {
-    const [startTime, setStartTime] = useState();
-    const [finishTime, setFinishTime] = useState();
-    const [data, setData] = useState([]);   // 가져온 전체 데이터
+    const [startTime, setStartTime] = useState("2024-01-01T00:00:00");
+    const [finishTime, setFinishTime] = useState("2024-01-01T00:01:00");
+    const [data, setData] = useState(defaultData);   // 가져온 전체 데이터
 
     const handleStartTimeChange = (e) => {
         setStartTime(e.target.value);
@@ -35,27 +70,24 @@ function GraphDashboard() {
 
             if (response.status === 200) {
                 console.log("그래프 데이터를 성공적으로 로드했습니다.");
+            
+                // 가져온 데이터들을 Data에 저장.
+                setData(response.data);
+                return
             } else {
                 errorAlert("그래프 데이터를 성공적으로 얻어오지 못 했습니다.");
                 return
             }
-
-            // 가져온 데이터들을 Data에 저장.
-            const result = await response.json();
-            setData(result);
-            
         } catch (error) {
             if (error.response) {
                 console.error("응답 오류: ", error.response.data);
                 console.error("응답 상태: ", error.response.status);
                 console.error("응답 헤더: ", error.response.headers);
-                errorAlert(`데이터 패치 실패: ${error.response.data}`);
               } else if (error.request) {
                 console.error("요청 오류: ", error.request);
                 errorAlert("서버 응답이 없습니다. 나중에 다시 시도해주세요.");
               } else {
                 console.error("데이터 요청 중 오류 발생: ", error.message);
-                errorAlert("데이터 요청 중 오류가 발생했습니다.");
             }
         }
     }, [startTime, finishTime, deviceId]);
@@ -65,10 +97,9 @@ function GraphDashboard() {
         fetchData();
     }, [fetchData])
 
-
     // Download 버튼 누를 시 전체 데이터 가져옴.
     const downloadData = () => {
-        const url = "http://localhost:8080/api/obdlog/~~~";
+        const url = "http://localhost:8080/api/obdlog/download";
         const parameter = `${url}?deviceId=${encodeURIComponent(deviceId)}&startDate=${encodeURIComponent(startTime)}&endDate=${encodeURIComponent(finishTime)}`;
         const response = axios.get(parameter, {
             headers: {
@@ -90,112 +121,42 @@ function GraphDashboard() {
  
     // 그래프 기본 옵션(SPEED에 기반해서 나머지 만듦)
     const options = {
-        chart: {
-          type: "area",
-          height: 350,
-          background: "#ffffff"
-        },
-        dataLabels: {
-          enabled: false
-        },
-        stroke: {
-          curve: "straight"
-        },
-        title: {
-          text: "SPEED", // 여기에 데이터 이름
-          align: "left"
-        },
-        subtitle: {
-          text: "km/h", // 여기에 데이터 단위
-          align: "left"
-        },
+        chart: { type: "area", height: 350, background: "#ffffff" },
+        dataLabels: { enabled: false },
+        stroke: { curve: "straight" },
+        title: { text: "SPEED", align: "left" },    // 데이터 이름
+        subtitle: { text: "km/h", align: "left" },  // 데이터 단위
         labels: [startTime, finishTime],
-        xaxis: {
-          type: "datetime",
-        },
-        yaxis: {
-          opposite: false,
-        },
-        legend: {
-          horizontalAlign: "left"
-        }
+        xaxis: { type: "datetime", },
+        yaxis: { opposite: false, },
+        legend: { horizontalAlign: "left" }
     };
 
-
     // 그래프별 세부 옵션
-    const rpm_options = {
-        ...options,
-        title: { text: "RPM" },
-        subtitle: { text: "rpm"}
-    }
+    const rpm_options = { ...options, title: { text: "RPM" }, subtitle: { text: "rpm"} }
+    const engine_load_options = { ...options, title: { text: "ENGINE LOAD" }, subtitle: { text: "percent(%)"} }
+    const throttle_pos_options = { ...options, title: { text: "THROTTLE POSITION" }, subtitle: { text: "" } }
+    const fuel_level_options = { ...options, title: { text: "FUEL LEVEL" }, subtitle: { text: "LEVEL"} }
+    const barometric_press_options = { ...options, title: { text: "BAROMETRIC PRESSURE" }, subtitle: { text: "℃"} }
+    const run_time_options = { ...options, title: { text: "RUN TIME" }, subtitle: { text: "TIME(DAY:HH:MM"} }
+    const distance_options = { ...options, title: { text: "DISTANCE" }, subtitle: { text: "℃"} }
+    const coolant_temp_options = { ...options, title: { text: "COOLANT TEMPERATURE" }, subtitle: { text: "℃"} }
 
-    const engine_load_options = {
-        ...options,
-        title: { text: "ENGINE LOAD" },
-        subtitle: { text: "percent(%)"}
-    }
+    // 데이터들 중 원하는 것만 취사 선택해서 그래프에 표시할 데이터로 포맷.
+    const getSeries = (key) => {
+        const series = [
+          {
+            name: key,
+            data: data.map((item) => ({
+              x: new Date(item.timeStamp).getTime(),
+              y: item[key], // 값이 없으면 0으로 초기화
+            })),
+          },
+        ];
+        return series;
+      };
 
-    const throttle_pos_options = {
-        ...options,
-        title: { text: "THROTTLE POSITION" },
-        subtitle: { text: "" }
-    }
-
-    const fuel_level_options = {
-        ...options,
-        title: { text: "FUEL LEVEL" },
-        subtitle: { text: "LEVEL"}
-    }
-
-    const barometric_press_options = {
-        ...options,
-        title: { text: "BAROMETRIC PRESSURE" },
-        subtitle: { text: "℃"}
-    }
-
-    const run_time_options = {
-        ...options,
-        title: { text: "RUN TIME" },
-        subtitle: { text: "TIME(DAY:HH:MM"}
-    }
-
-    const distance_options = {
-        ...options,
-        title: { text: "DISTANCE" },
-        subtitle: { text: "℃"}
-    }
-    
-    const coolant_temp_options = {
-        ...options,
-        title: { text: "COOLANT TEMPERATURE" },
-        subtitle: { text: "℃"}
-    }
-
-    // 데이터 형식 통일 및 시간 데이터 처리
-    const formattedData = data.map(entry => ({
-        x: new Date(entry.timeStamp).getTime(),
-        speed: entry.speed,
-        rpm: entry.rpm,
-        engineLoad: entry.engineLoad,
-        throttlePos: entry.throttlePos,
-        fuelLevel: entry.fuelLevel,
-        barometricPressure: entry.barometricPressure,
-        runTime: entry.runTime,
-        distance: entry.distance,
-        coolantTemp: entry.coolantTemp
-    }));
-
-    // x, y축에 맞춤.
-    const speedSeries = { name: 'Speed', data: formattedData.map(entry => ({ x: entry.x, y: entry.speed })) };
-    const rpmSeries = { name: 'RPM', data: formattedData.map(entry => ({ x: entry.x, y: entry.rpm })) };
-    const engineLoadSeries = { name: 'Engine Load', data: formattedData.map(entry => ({ x: entry.x, y: entry.engineLoad })) };
-    const throttlePosSeries = { name: 'Throttle Position', data: formattedData.map(entry => ({ x: entry.x, y: entry.throttlePos })) };
-    const fuelLevelSeries = { name: 'Fuel Level', data: formattedData.map(entry => ({ x: entry.x, y: entry.fuelLevel })) };
-    const barometricPressureSeries = { name: 'Barometric Pressure', data: formattedData.map(entry => ({ x: entry.x, y: entry.barometricPressure })) };
-    const runTimeSeries = { name: 'Run Time', data: formattedData.map(entry => ({ x: entry.x, y: entry.runTime })) };
-    const distanceSeries = { name: 'Distance', data: formattedData.map(entry => ({ x: entry.x, y: entry.distance })) };
-    const coolantTempSeries = { name: 'Coolant Temperature', data: formattedData.map(entry => ({ x: entry.x, y: entry.coolantTemp })) };
-
+    console.log(getSeries("speed"));
 
     return (
     <div className="Graphs">
@@ -223,19 +184,18 @@ function GraphDashboard() {
             </div>
 
             <div className="body">
-                <Chart options={options} series={[speedSeries]} type="area" height={350}/>
-                <Chart options={rpm_options} series={[rpmSeries]} type="area" height={350}/>
-                <Chart options={engine_load_options} series={[engineLoadSeries]} type="area" height={350}/>
-                <Chart options={throttle_pos_options} series={[throttlePosSeries]} type="area" height={350}/>
-                <Chart options={fuel_level_options} series={[fuelLevelSeries]} type="area" height={350}/>
-                <Chart options={barometric_press_options} series={[barometricPressureSeries]} type="area" height={350}/>
-                <Chart options={runTimeSeries} series={[run_time_options]} type="area" height={350}/>
-                <Chart options={distance_options} series={[distanceSeries]} type="area" height={350}/>
-                <Chart options={coolant_temp_options} series={[coolantTempSeries]} type="area" height={350}/>
+                <Chart options={options} series={getSeries("speed")} type="area" height={350}/>
+                <Chart options={rpm_options} series={getSeries("rpm")} type="area" height={350}/>
+                <Chart options={engine_load_options} series={getSeries("engineLoad")} type="area" height={350}/>
+                <Chart options={throttle_pos_options} series={getSeries("throttlePos")} type="area" height={350}/>
+                <Chart options={fuel_level_options} series={getSeries("fuelLevel")} type="area" height={350}/>
+                <Chart options={barometric_press_options} series={getSeries("barometricPress")} type="area" height={350}/>
+                <Chart options={run_time_options} series={getSeries("runTime")} type="area" height={350}/>
+                <Chart options={distance_options} series={getSeries("distance")} type="area" height={350}/>
+                <Chart options={coolant_temp_options} series={getSeries("coolantTemp")} type="area" height={350}/>
             </div>
         </div>
     </div>
-
     );
 }
 
